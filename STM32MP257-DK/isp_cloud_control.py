1d5016ede8d7bb6c100e917ab158923ee0e93063
mlamp99 <michael.lamp@avnet.com>
2025-05-06 12:45:15 -0500

    Update isp_cloud_control.py
    Enhance the commands and telemetry options
    
    Signed-off-by: mlamp99 <michael.lamp@avnet.com>

--- STM32MP257-DK/isp_cloud_control.py
+++ STM32MP257-DK/isp_cloud_control.py
@@ -7,16 +7,12 @@ import sys
 import time
 import gi
 import threading
-
 from avnet.iotconnect.sdk.lite import Client, DeviceConfig, C2dCommand, Callbacks, DeviceConfigError
-from avnet.iotconnect.sdk.lite import __version__ as SDK_VERSION
 from avnet.iotconnect.sdk.sdklib.mqtt import C2dAck
-
-import re
+from avnet.iotconnect.sdk.lite import __version__ as SDK_VERSION
 
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
-
 Gst.init(None)
 
 pipeline = None
@@ -24,15 +20,11 @@ src = None
 
 def start_pipeline():
     global pipeline, src
-
     pipeline_desc = ("libcamerasrc name=src ! video/x-raw,format=I420,width=1280,height=720 "
                      "! videoconvert ! autovideosink sync=false")
-
     pipeline = Gst.parse_launch(pipeline_desc)
     pipeline.set_state(Gst.State.PLAYING)
-
     time.sleep(2)
-
     src = pipeline.get_by_name("src")
     if src:
         src.set_property("statistic-profile", 0)
@@ -57,7 +49,23 @@ def read_isp_stats():
         histogram_vals = list(src.get_property("statistic-get-histogram-down"))
 
         stats = {
+            "sdk_version": SDK_VERSION,
             "exposure": int(src.get_property("sensor-exposure")),
+            "sensor_gain": float(src.get_property("sensor-gain")),
+            "isp_gain": {
+                "R": float(src.get_property("isp-gain-values")[0]),
+                "G": float(src.get_property("isp-gain-values")[1]),
+                "B": float(src.get_property("isp-gain-values")[2])
+            },
+            "black_level": {
+                "R": int(src.get_property("black-level-values")[0]),
+                "G": int(src.get_property("black-level-values")[1]),
+                "B": int(src.get_property("black-level-values")[2])
+            },
+            "aec_enabled": bool(src.get_property("aec-algo-enable")),
+            "aec_exposure_compensation": float(src.get_property("aec-algo-exposure-compensation")),
+            "awb_enabled": bool(src.get_property("awb-algo-enable")),
+            "demosaicing_enabled": bool(src.get_property("demosaicing-enable")),
             "luminance": {
                 "lum_0": contrast_vals[0],
                 "lum_32": contrast_vals[1],
@@ -82,7 +90,6 @@ def read_isp_stats():
 
 def on_command(msg: C2dCommand):
     global src
-
     print("Received command", msg.command_name, msg.command_args, msg.ack_id)
 
     if msg.command_name == "start-stream":
@@ -103,16 +110,67 @@ def on_command(msg: C2dCommand):
 
     elif msg.command_name == "set-contrast":
         if src:
-            try:
-                contrast = [int(x) for x in msg.command_args[0].split()]
-                if len(contrast) == 9:
-                    src.set_property("contrast-enable", True)
-                    src.set_property("contrast-values", contrast)
-                    c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "Contrast adjusted")
-                else:
-                    c.send_command_ack(msg, C2dAck.CMD_FAILED, "Contrast requires 9 values")
-            except ValueError:
-                c.send_command_ack(msg, C2dAck.CMD_FAILED, "Invalid contrast values")
+            contrast = [int(x) for x in msg.command_args]
+
+            if len(contrast) == 9:
+                src.set_property("contrast-enable", True)
+                src.set_property("contrast-values", contrast)
+                c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "Contrast adjusted")
+            else:
+                c.send_command_ack(msg, C2dAck.CMD_FAILED, "Contrast requires 9 values")
+
+    elif msg.command_name == "set-gain":
+        if src and len(msg.command_args) == 1:
+            src.set_property("sensor-gain", float(msg.command_args[0]))
+            c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "Gain set")
+        else:
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "Gain command error")
+
+    elif msg.command_name == "set-awb":
+        if src and msg.command_args[0] in ("enable", "disable"):
+            src.set_property("awb-algo-enable", msg.command_args[0] == "enable")
+            c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "AWB set")
+        else:
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "AWB command error")
+
+    elif msg.command_name == "set-black-level":
+        if src and len(msg.command_args) == 3:
+            bl = [int(x) for x in msg.command_args]
+            src.set_property("black-level-values", bl)
+            src.set_property("black-level-enable", True)
+            c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "Black level set")
+        else:
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "Black level command error")
+
+    elif msg.command_name == "set-isp-gain":
+        if src and len(msg.command_args) == 3:
+            isp_gain = [float(x) for x in msg.command_args]
+            src.set_property("isp-gain-values", isp_gain)
+            src.set_property("isp-gain-enable", True)
+            c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "ISP gain set")
+        else:
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "ISP gain command error")
+
+    elif msg.command_name == "set-aec":
+        if src and msg.command_args[0] in ("enable", "disable"):
+            src.set_property("aec-algo-enable", msg.command_args[0] == "enable")
+            c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "AEC set")
+        else:
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "AEC command error")
+
+    elif msg.command_name == "set-aec-compensation":
+        if src and len(msg.command_args) == 1:
+            src.set_property("aec-algo-exposure-compensation", float(msg.command_args[0]))
+            c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "AEC compensation set")
+        else:
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "AEC compensation command error")
+
+    elif msg.command_name == "set-demosaicing":
+        if src and msg.command_args[0] in ("enable", "disable"):
+            src.set_property("demosaicing-enable", msg.command_args[0] == "enable")
+            c.send_command_ack(msg, C2dAck.CMD_SUCCESS_WITH_ACK, "Demosaicing set")
+        else:
+            c.send_command_ack(msg, C2dAck.CMD_FAILED, "Demosaicing command error")
 
     elif msg.command_name == "get-isp-settings":
         stats = read_isp_stats()
@@ -122,11 +180,9 @@ def on_command(msg: C2dCommand):
         else:
             c.send_command_ack(msg, C2dAck.CMD_FAILED, "Pipeline not started")
 
-    else:
-        c.send_command_ack(msg, C2dAck.CMD_FAILED, "Not Implemented")
-
 def on_disconnect(reason: str, disconnected_from_server: bool):
-    print("Disconnected%s. Reason: %s" % (" from server" if disconnected_from_server else "", reason))
+    print("Disconnected:", reason)
+
 
 try:
     device_config = DeviceConfig.from_iotc_device_config_json_file(
